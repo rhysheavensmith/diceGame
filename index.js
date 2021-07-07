4166fe259bcaa5a3eb2ce805d516ba7e6adfc57a
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2021-07-07 12:59:37 +1200

    UX

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-var randomDice1 = "images/dice" + randomNumber1 + ".png";
+let randomDice1 = "images/dice" + randomNumber1 + ".png";
 
-var randomDice2 = "images/dice" + randomNumber2 + ".png";
+let randomDice2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
 
